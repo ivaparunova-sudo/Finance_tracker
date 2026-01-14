f4bd75d55aab27087970e42c633ae63dd538130e
Iva Parunova <iva.parunova@gmail.com>
2026-01-14 19:01:58 +0200

    Добавена функция Report() и създаден нов глобален масив
    Създадох нов глобален масив monthArr[], който съхранява кои поредни месеци от годината ще бъдат обработвани от програмата. Добавих функция Report(), която генерира отчет за избраните от потребителя месеци въз основа на въведения инпут. Чрез нея се визуализират приходите, разходите и балансът за всеки месец, както и общите приходи и разходи, заедно със средния месечен баланс.

--- Finance_tracker.cpp
+++ Finance_tracker.cpp
@@ -15,12 +15,15 @@
 
 
 #include <iostream>
+#include <string>
 
 int numberOfMonths = 1; //The number of months for which the program will generate a financial report.
 bool ranCorrectly = false;
 float incomeArr[12] = { 0 };
 float expenseArr[12] = { 0 };
 float balanceArr[12] = { 0 };
+int monthArr[12] = { 0 }; //An array containing the months that the program is working with.
+
 
 void Setup(int& numberOfMonths) //Initializes a new financial profile for a number of months specified by the user.
 {
@@ -56,7 +59,7 @@ void TurnToLet(int month) //Transforms an integer from 1-12 into the designated 
 	std::cout << monthInLet;
 }
 
-void Add(int numberOfMonths, float incomeArr[], float expenseArr[], float balanceArr[]) //Takes the user's input and stores it in the designated arreys. 
+void Add(int numberOfMonths, int monthArr[], float incomeArr[], float expenseArr[], float balanceArr[]) //Takes the user's input and stores it in order of the months in the designated arreys. 
 {
 	int count = 0; 
 	while (count < numberOfMonths)
@@ -64,7 +67,7 @@ void Add(int numberOfMonths, float incomeArr[], float expenseArr[], float balanc
 		int month;
 		float expense, income;
 
-		std::cout << "Enter month (1-12): ";
+		std::cout << "Enter month (1-12): "; //The number represents the month’s position within the year.
 		std::cin >> month;
 
 		while (month < 1 || month > 12)
@@ -97,9 +100,11 @@ void Add(int numberOfMonths, float incomeArr[], float expenseArr[], float balanc
 		std::cout << " = ";
 		if (balance > 0) std::cout << "+" << balance << std::endl;
 		else std::cout << balance << std::endl;
+		std::cout << std::endl;
 		incomeArr[month - 1] = income;
 		expenseArr[month - 1] = expense;
 		balanceArr[month - 1] = balance;
+		monthArr[month - 1] = month;
 
 		count++;
 	}
@@ -107,9 +112,48 @@ void Add(int numberOfMonths, float incomeArr[], float expenseArr[], float balanc
 	ranCorrectly = true;
 }
 
+
+void Report(float incomeArr[], float expenseArr[], float balanceArr[])
+{
+	float total_income = 0;
+	float total_expense = 0;
+	float total_balance = 0;
+	for (int i = 0; i < numberOfMonths; i++)
+	{
+		total_income += incomeArr[i];
+	}
+	for (int i = 0; i < numberOfMonths; i++)
+	{
+		total_expense += expenseArr[i];
+	}
+	for (int i = 0; i < numberOfMonths; i++)
+	{
+		total_balance += balanceArr[i];
+	}
+	float average_balance = (total_balance / numberOfMonths);
+	std::cout << "   Month    |   Income   |   Expense   |   Balance  " << std::endl;
+	std::cout << "----------------------------------------------------" << std::endl;
+	for (int i = 0; i < numberOfMonths; i++)
+	{
+		TurnToLet(monthArr[0]);
+		std::cout << " | " << incomeArr[i] << " | " << expenseArr[i] << " | ";
+		if (balanceArr[i] > 0) std::cout << "+" << balanceArr[i] << std::endl;
+		else std::cout << balanceArr[i] << std::endl;
+	}
+	std::cout << "----------------------------------------------------" << std::endl;
+	std::cout << "Total income: " << total_income << std::endl;
+	std::cout << "Total expense: " << total_expense << std::endl;
+	std::cout << "Average balance: ";
+	if (average_balance > 0) std::cout << "+" << average_balance << std::endl;
+	else std::cout << average_balance << std::endl;
+}
+
+
 int main()
 {
 	Setup(numberOfMonths);
-	Add(numberOfMonths, incomeArr, expenseArr, balanceArr));
+	Add(numberOfMonths, monthArr, incomeArr, expenseArr, balanceArr);
+	Report(incomeArr, expenseArr, balanceArr);
+
 	return 0;
 }
